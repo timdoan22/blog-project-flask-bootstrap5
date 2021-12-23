22b08a235c0a66de97bde26095cec1124dc717a7
timdoan22 <timdoan01@gmail.com>
2021-12-23 15:12:00 -0500

    Incorporated blog posts from a get request and returned it to be displayed on index.html in a jinja loop dynamically; added a background image in the index.html page

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import requests
+
+blog_posts = requests.get('https://gist.githubusercontent.com/gellowg/389b1e4d6ff8effac71badff67e4d388/raw/fc31e41f8e1a6b713eafb9859f3f7e335939d518/').json()
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', blog_posts=blog_posts)
 
 @app.route('/about')
 def about():
