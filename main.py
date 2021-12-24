fa3a657bee88e54ddffe50994fdf91d1c5a50351
timdoan22 <timdoan01@gmail.com>
2021-12-24 12:21:01 -0500

    Setup send_email functionality using smtplib to send out an email with the form data

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template, request
 import requests
+import os
+import smtplib
+
+MY_EMAIL = os.environ.get("TEST_EMAIL")
+EMAIL_PASSWORD = os.environ.get("TEST_EMAIL_PASSWORD")
+TEST_EMAIL = os.environ.get("DUMMY_EMAIL")
 
 blog_posts = requests.get('https://gist.githubusercontent.com/gellowg/389b1e4d6ff8effac71badff67e4d388/raw/fc31e41f8e1a6b713eafb9859f3f7e335939d518/').json()
+
+def send_email(recipient_name, recipient_email, recipient_phone, message):
+    recipient_name = recipient_name.capitalize()
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=EMAIL_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=TEST_EMAIL,
+            msg=f"Subject:{recipient_name} Has Sent you a Message!\n\nThe following message was sent by:\n\nName: {recipient_name}\nEmail: {recipient_email}\nPhone: {recipient_phone}\nMessage: {message}"
+        )
 
 app = Flask(__name__)
 
@@ -29,10 +46,7 @@ def get_blog(index):
 def receive_data():
     if request.method == 'POST':
         data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])
+        send_email(data["name"], data["email"], data["phone"], data["message"])
         return render_template("contact.html", result=True)
     return render_template("contact.html", result=False)
 
